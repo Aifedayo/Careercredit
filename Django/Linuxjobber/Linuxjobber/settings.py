f3231cfa763a84baeea8c71e52f98fb0b3af806b
Animashaun Abdul Azeem <azeemanimashaun@gmail.com>
2018-12-03 22:24:55 +0100

    STATIC_ROOT mod

--- Django/Linuxjobber/Linuxjobber/settings.py
+++ Django/Linuxjobber/Linuxjobber/settings.py
@@ -232,7 +232,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'asset'),
