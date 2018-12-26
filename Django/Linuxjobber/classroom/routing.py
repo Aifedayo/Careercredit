3795cacf4368bafa0cc7649ad3e1f44b01489f6a
Animashaun Abdul Azeem <azeemanimashaun@gmail.com>
2018-12-26 04:07:09 +0100

    Setup new websocket module

--- Django/Linuxjobber/classroom/routing.py
+++ Django/Linuxjobber/classroom/routing.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.urls import path
 
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r'', consumers.ChatConsumer)
+    # url(r'', consumers.ChatConsumer)
+    path('<int:group_id>', consumers.ChatConsumer)
 ]
