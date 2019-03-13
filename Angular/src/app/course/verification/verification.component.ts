9e4e00c2fae76323e47cde09a0850782ea9dccbf
Animashaun Abdul Azeem <azeemanimashaun@gmail.com>
2019-03-13 09:18:27 +0100

    Video Component (1)

--- Angular/src/app/course/verification/verification.component.ts
+++ Angular/src/app/course/verification/verification.component.ts
@@ -1,152 +1,117 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ElementRef,
+  Input
+} from '@angular/core';
+
+import videojs from 'video.js';
+import * as adapter from 'webrtc-adapter/out/adapter_no_global.js';
+import * as RecordRTC from 'recordrtc';
+
+// register videojs-record plugin with this import
+import * as Record from 'videojs-record/dist/videojs.record.js';
 
 @Component({
-  selector: 'app-verification',
+  selector: 'videojs-record',
   templateUrl: './verification.component.html',
   styleUrls: ['./verification.component.css']
 })
-export class VerificationComponent implements OnInit {
 
-//   constructor() {
-//
-//     const mediaSource = new MediaSource();
-// mediaSource.addEventListener('sourceopen', handleSourceOpen, false);
-// let mediaRecorder;
-// let recordedBlobs;
-// let sourceBuffer;
-//
-// const errorMsgElement = document.querySelector('span#errorMsg');
-// const recordedVideo = document.querySelector('video#recorded');
-// const recordButton = document.querySelector('button#record');
-// recordButton.addEventListener('click', () => {
-//   if (recordButton.textContent === 'Start Recording') {
-//     startRecording();
-//   } else {
-//     stopRecording();
-//     recordButton.textContent = 'Start Recording';
-//     playButton.disabled = false;
-//     downloadButton.disabled = false;
-//   }
-// });
-//
-// const playButton = document.querySelector('button#play');
-// playButton.addEventListener('click', () => {
-//   const superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
-//   recordedVideo.src = null;
-//   recordedVideo.srcObject = null;
-//   recordedVideo.src = window.URL.createObjectURL(superBuffer);
-//   recordedVideo.controls = true;
-//   recordedVideo.play();
-// });
-//
-// const downloadButton = document.querySelector('button#download');
-// downloadButton.addEventListener('click', () => {
-//   const blob = new Blob(recordedBlobs, {type: 'video/webm'});
-//   const url = window.URL.createObjectURL(blob);
-//   const a = document.createElement('a');
-//   a.style.display = 'none';
-//   a.href = url;
-//   a.download = 'test.webm';
-//   document.body.appendChild(a);
-//   a.click();
-//   setTimeout(() => {
-//     document.body.removeChild(a);
-//     window.URL.revokeObjectURL(url);
-//   }, 100);
-// });
-//
-// function handleSourceOpen(event) {
-//   console.log('MediaSource opened');
-//   sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
-//   console.log('Source buffer: ', sourceBuffer);
-// }
-//
-// function handleDataAvailable(event) {
-//   if (event.data && event.data.size > 0) {
-//     recordedBlobs.push(event.data);
-//   }
-// }
-//
-// function startRecording() {
-//   recordedBlobs = [];
-//   let options = {mimeType: 'video/webm;codecs=vp9'};
-//   if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-//     console.error(`${options.mimeType} is not Supported`);
-//     errorMsgElement.innerHTML = `${options.mimeType} is not Supported`;
-//     options = {mimeType: 'video/webm;codecs=vp8'};
-//     if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-//       console.error(`${options.mimeType} is not Supported`);
-//       errorMsgElement.innerHTML = `${options.mimeType} is not Supported`;
-//       options = {mimeType: 'video/webm'};
-//       if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-//         console.error(`${options.mimeType} is not Supported`);
-//         errorMsgElement.innerHTML = `${options.mimeType} is not Supported`;
-//         options = {mimeType: ''};
-//       }
-//     }
-//   }
-//
-//   try {
-//     mediaRecorder = new MediaRecorder(window.stream, options);
-//   } catch (e) {
-//     console.error('Exception while creating MediaRecorder:', e);
-//     errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
-//     return;
-//   }
-//
-//   console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
-//   recordButton.textContent = 'Stop Recording';
-//   playButton.disabled = true;
-//   downloadButton.disabled = true;
-//   mediaRecorder.onstop = (event) => {
-//     console.log('Recorder stopped: ', event);
-//   };
-//   mediaRecorder.ondataavailable = handleDataAvailable;
-//   mediaRecorder.start(10); // collect 10ms of data
-//   console.log('MediaRecorder started', mediaRecorder);
-// }
-//
-// function stopRecording() {
-//   mediaRecorder.stop();
-//   console.log('Recorded Blobs: ', recordedBlobs);
-// }
-//
-// function handleSuccess(stream) {
-//   recordButton.disabled = false;
-//   console.log('getUserMedia() got stream:', stream);
-//   window.stream = stream;
-//
-//   const gumVideo = document.querySelector('video#gum');
-//   gumVideo.srcObject = stream;
-// }
-//
-// async function init(constraints) {
-//   try {
-//     const stream = await navigator.mediaDevices.getUserMedia(constraints);
-//     handleSuccess(stream);
-//   } catch (e) {
-//     console.error('navigator.getUserMedia error:', e);
-//     errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
-//   }
-// }
-//
-// document.querySelector('button#start').addEventListener('click', async () => {
-//   const hasEchoCancellation = document.querySelector('#echoCancellation').checked;
-//   const constraints = {
-//     audio: {
-//       echoCancellation: {exact: hasEchoCancellation}
-//     },
-//     video: {
-//       width: 1280, height: 720
-//     }
-//   };
-//   console.log('Using media constraints:', constraints);
-//   await init(constraints);
-// });
-//
-//   }
+export class VerificationComponent implements OnInit, OnDestroy {
 
-  ngOnInit() {
+  // reference to the element itself: used to access events and methods
+  private _elementRef: ElementRef
+
+  // index to create unique ID for component
+  @Input() idx: string;
+
+  private config: any;
+  private player: any;
+  private plugin: any;
+
+  // constructor initializes our declared vars
+  constructor(elementRef: ElementRef) {
+    this.player = false;
+
+    // save reference to plugin (so it initializes)
+    this.plugin = Record;
+
+    // video.js configuration
+    this.config = {
+      controls: true,
+      autoplay: false,
+      fluid: false,
+      loop: false,
+      width: 320,
+      height: 240,
+      controlBar: {
+        volumePanel: false
+      },
+      plugins: {
+        // configure videojs-record plugin
+        record: {
+          audio: false,
+          video: true,
+          debug: true
+        }
+      }
+    };
+  }
+
+  ngOnInit() {}
+
+  // use ngAfterViewInit to make sure we initialize the videojs element
+  // after the component template itself has been rendered
+  ngAfterViewInit() {
+    // ID with which to access the template's video element
+    let el = 'video_' + this.idx;
+
+    // setup the player via the unique element ID
+    this.player = videojs(document.getElementById(el), this.config, () => {
+      console.log('player ready! id:', el);
+
+      // print version information at startup
+      var msg = 'Using video.js ' + videojs.VERSION +
+        ' with videojs-record ' + videojs.getPluginVersion('record') +
+        ' and recordrtc ' + RecordRTC.version;
+      videojs.log(msg);
+    });
+
+    // device is ready
+    this.player.on('deviceReady', () => {
+      console.log('device is ready!');
+    });
+
+    // user clicked the record button and started recording
+    this.player.on('startRecord', () => {
+      console.log('started recording!');
+    });
+
+    // user completed recording and stream is available
+    this.player.on('finishRecord', () => {
+      // recordedData is a blob object containing the recorded data that
+      // can be downloaded by the user, stored on server etc.
+      console.log('finished recording: ', this.player.recordedData);
+    });
+
+    // error handling
+    this.player.on('error', (element, error) => {
+      console.warn(error);
+    });
+
+    this.player.on('deviceError', () => {
+      console.error('device error:', this.player.deviceErrorCode);
+    });
+  }
+
+  // use ngOnDestroy to detach event handlers and remove the player
+  ngOnDestroy() {
+    if (this.player) {
+      this.player.dispose();
+      this.player = false;
+    }
   }
 
 }
