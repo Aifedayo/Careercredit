ea97972989007d17e2fbf7de9b12814f2af0ed68
Animashaun Abdul Azeem <azeemanimashaun@gmail.com>
2019-08-09 09:25:37 +0100

    Url fix

--- Django/Linuxjobber/Courses/urls.py
+++ Django/Linuxjobber/Courses/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', views.courses,name='courses'),
     # path('<slug:course_name>/topics/', login_required(views.CourseTopicsView.as_view()), name='topics'),
     # path('<slug:course_name>/topic/<int:lab_no>/', views.topicdetails, name='topic_details'),
-    path('<slug:course_name>/notes/<int:lab_no>/', views.TopicNote, name='Note'),
+    # path('<slug:course_name>/notes/<int:lab_no>/', views.TopicNote, name='Note'),
     path('<slug:course_name>/labs/<int:lab_no>/', login_required(views.LabDetailsView.as_view()), name='labs'),
     path('userinterest', views.userinterest, name='userinterest'),
     path('description/<slug:course_name>', views.description, name='description'),
@@ -28,5 +28,8 @@ urlpatterns = [
     path('<slug:course_name>/labprofiledetail/<int:lab_no>', views.labprofiledetail, name='labprofiledetail'),
     path('<slug:course_name>/topics/', views.self_topic_index, name='topics'),
     path('<slug:course_name>/topic/<int:lab_no>/', views.self_topic_details, name='topic_details'),
+    # Notes is referred via a div
+    path('<slug:course_name>/topic/<int:lab_no>/notes', views.self_topic_details, name='Note'),
+
 
 ]
