8db9b510e27eb128cb26166ec40f771a685d6665
Akorede Fodilu Olawale <afordeal88@gmail.com>
2020-05-13 09:59:12 +0100

    commit migrations

--- Django/Linuxjobber/home/admin.py
+++ Django/Linuxjobber/home/admin.py
@@ -21,7 +21,7 @@ from .forms import  UpcomingScheduleForm
 from .mail_service import LinuxjobberMassMailer, handle_campaign, LinuxjobberMailer
 from .models import FAQ, Job, RHCSAOrder, FreeAccountClick, Campaign, Message, Unsubscriber, Internship, \
     InternshipDetail, MessageGroup, UserLocation, NewsLetterSubscribers, UserOrder, Document, MainModel, AwsCredential, \
-    Jobplacement, Groupclass,BillingHistory, PaymentHistory, GroupClassRegister, StripePayment, UserPayment, wepeoples, wetask, werole, \
+    Jobplacement, Groupclass, BillingHistory, PaymentHistory, GroupClassRegister, StripePayment, UserPayment, wepeoples, wetask, werole, \
     wework, wetype, PartTimeJob, TryFreeRecord, FullTimePostion, PartTimePostion, Resume, CareerSwitchApplication, \
     Certificates, EmailMessageType, EmailMessageLog, CompleteClass, \
     CompleteClassLearn, CompleteClassCertificate, WorkExperienceEligibility, WorkExperienceIsa, WorkExperiencePay, \
