893bc87732bc78b02f1b81bd05ccdce6a2d42dd2
Akorede Fodilu Olawale <afordeal88@gmail.com>
2020-06-07 11:19:54 +0100

    commit changes

--- Angular/src/app/admin-dashboard/students/students.component.ts
+++ Angular/src/app/admin-dashboard/students/students.component.ts
@@ -34,20 +34,19 @@ export class StudentsComponent implements OnInit {
           this.attendance = this.apiService.getUserAttendance(sessionStorage.getItem('active_group'), user_id)
           
           this.attendance.subscribe(b=>{
-            b.map(c =>{
+            const last_login = b[0]
+            // console.log(last_login)
               // console.log(c)  
-              let last_login = c.timestamp.slice(0, -10)
-              console.log(last_login)
-              
-              if (last_login == date.value){
-                // console.log('hi')
-                this.apiService.deleteUser(sessionStorage.getItem('active_group'), user_id).subscribe(
-                  data => {
-                   console.log()
-                  }
-                )
-              }
-            })
+            const last_login_date = last_login.timestamp.slice(0, -10)
+            if (last_login_date == date.value){
+              // console.log('hi')
+              this.apiService.deleteUser(sessionStorage.getItem('active_group'), user_id).subscribe(
+                data => {
+                  console.log()
+                }
+              )
+            }
+            
           })
               
             }
