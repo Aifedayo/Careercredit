4398956e6768f8ecb72b802fee6ae2489aa68a89
Akorede Fodilu Olawale <afordeal88@gmail.com>
2020-06-14 13:43:11 +0100

    commit student delete

--- Angular/src/app/admin-dashboard/students/students.component.ts
+++ Angular/src/app/admin-dashboard/students/students.component.ts
@@ -3,6 +3,7 @@ import {ApiService} from "../../share/api.service";
 import {UserModel} from "../../share/user-model";
 import {Observable} from "rxjs/index";
 import { stringify } from 'querystring';
+import {map, tap} from "rxjs/operators"
 
 @Component({
   selector: 'app-students',
@@ -26,47 +27,82 @@ export class StudentsComponent implements OnInit {
   
 
   deleteByDate(date: HTMLInputElement){
-    // let dt = new Date('Tuesday 17 December, 2019')
-    // let pdt = new Date('2020-06-10')
-    // console.log(date.valueAsDate)
-    // console.log(dt)
-    // 
-    this.students.subscribe(
-      student =>{
-        
-        student.map(a =>{
-          let user_id =a['id']
-          this.attendance = this.apiService.getUserAttendance(sessionStorage.getItem('active_group'), user_id)
-          
-          this.attendance.subscribe(b=>{
-            const last_login = b[0]
-            if(last_login){
-                // console.log(last_login)
-                // console.log(c)  
-              const last_login_date = new Date( last_login.timestamp.slice(0, -10))
-              // console.log(last_login_date)
-              // console.log(date.valueAsDate)
+    let delete_stds = this.students.pipe(
+      tap(val=>{
+      val.map(a=> {
+        let user_id =a['id']
+        this.attendance = this.apiService.getUserAttendance(sessionStorage.getItem('active_group'), user_id).
+        pipe(tap(b=>{
+          const last_login = b[0]
+          if(last_login){
+            console.log(last_login)
+            const last_login_date = new Date( last_login.timestamp.slice(0, -10))
               if (date.valueAsDate >= last_login_date){
-                // console.log('hi')
                 this.apiService.deleteUser(sessionStorage.getItem('active_group'), user_id).subscribe(
                   data => {
                     console.log()
                   }
                 )
-              } 
-            }
+              }
+          }
+          return(last_login)
+        }))
+        this.attendance.subscribe(res=>{
+          console.log()
+        })
+        
+      })
 
-          })
-              
-            }
-          )
-          setTimeout(()=>{
-            alert('Students has been removed')
-            window.location.replace(window.location.origin+"/admin/students")
-          }, 2000)
-
-      }
+    })
     )
+    delete_stds.subscribe(
+       ()=> {},
+       ()=> {},
+       ()=> {
+        setTimeout(()=>{
+                  alert('Students has been removed')
+                  window.location.replace(window.location.origin+"/admin/students")
+                }, 2000)
+       },
+  )
+  //NEW IMPLEMENTATION ABOVE
+    // this.students.subscribe(res=>{
+    //   console.log(res)
+    // })
+    // this.students.subscribe(
+    //   student =>{
+    //     student.map(a =>{
+    //       let user_id =a['id']
+    //       this.attendance = this.apiService.getUserAttendance(sessionStorage.getItem('active_group'), user_id)
+          
+    //       this.attendance.subscribe(b=>{
+    //         const last_login = b[0]
+    //         if(last_login){
+    
+    //           const last_login_date = new Date( last_login.timestamp.slice(0, -10))
+         
+    //           if (date.valueAsDate >= last_login_date){
+    //             this.apiService.deleteUser(sessionStorage.getItem('active_group'), user_id).subscribe(
+    //               data => {
+    //                 console.log()
+    //               }
+    //             )
+    //           } 
+    //         }
+
+    //       })
+              
+    //         }
+    //       )
+    //   },
+    //   ()=>{},
+    //   ()=> {
+    //     setTimeout(()=>{
+    //       alert('Students has been removed')
+    //       window.location.replace(window.location.origin+"/admin/students")
+    //     }, 2000)
+    //   }
+    // )
 
   }
 }
